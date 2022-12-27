987e26ade21e1a6b9ed55aba90d22c7c535a4d61
coco-dogge <brian123483@yahoo.com.tw>
2022-12-28 01:43:18 +0800

    error

--- app/src/main/java/com/example/rttl_13/AD.java
+++ app/src/main/java/com/example/rttl_13/AD.java
@@ -1,22 +1,70 @@
 package com.example.rttl_13;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class AD extends AppCompatActivity {
 
-
+        private AdView mdView;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.ad);
 
+            MobileAds.initialize(this, initializationStatus -> {
+            });
 
+
+            mdView = findViewById(R.id.adView);
+            AdRequest adRequest = new AdRequest.Builder().build();
+            mdView.loadAd(adRequest);/*
+            mdView.setAdListener(new AdListener() {
+                @Override
+                public void onAdClicked() {
+                    // Code to be executed when the user clicks on an ad.
+                }
+
+                @Override
+                public void onAdClosed() {
+                    super.onAdClosed();
+                    adView.loadAd(adRequest);
+                    // Code to be executed when the user is about to return
+                    // to the app after tapping on an ad.
+                }
+
+                @Override
+                public void onAdFailedToLoad(LoadAdError adError) {
+                    // Code to be executed when an ad request fails.
+                }
+
+                @Override
+                public void onAdImpression() {
+                    // Code to be executed when an impression is recorded
+                    // for an ad.
+                }
+
+                @Override
+                public void onAdLoaded() {
+                    // Code to be executed when an ad finishes loading.
+                }
+
+                @Override
+                public void onAdOpened() {
+                    // Code to be executed when an ad opens an overlay that
+                    // covers the screen.
+                }
+            });*/
         }
+
     }
--- app/src/main/java/com/example/rttl_13/MainActivity.java
+++ app/src/main/java/com/example/rttl_13/MainActivity.java
@@ -2,32 +2,26 @@ package com.example.rttl_13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AdView mAdView;
+     Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
+
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener((View v)-> {
+                Intent intent = new Intent(this, AD.class);
+                startActivity(intent);
         });
-
-        mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-
     }
 }
