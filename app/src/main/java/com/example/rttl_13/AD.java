e93842ef970ebe8efd0fc38261d5867ca3132429
coco-dogge <brian123483@yahoo.com.tw>
2022-12-27 22:26:29 +0800

    ad_first

---
+++ app/src/main/java/com/example/rttl_13/AD.java
@@ -0,0 +1,22 @@
+package com.example.rttl_13;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class AD extends AppCompatActivity {
+
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.ad);
+
+
+        }
+    }
