940546e5661127003675d65e2c58073ba2c7b6f9
SHEN XIAN KUN <106544049+popstr-kun@users.noreply.github.com>
2022-12-29 02:21:31 +0800

    語音識別與翻譯測試_語言切換測試

--- app/src/main/java/com/example/rttl_13/MainActivity.java
+++ app/src/main/java/com/example/rttl_13/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "zh-TW");
                 intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "請說～");
                 try{
                     startActivityForResult(intent,200);
@@ -60,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 TranslateOptions options = TranslateOptions.newBuilder().setApiKey("AIzaSyB1t4w5AJ3A2fOOacSWbYjj7peFyIXoYyg").build();
                 Translate translate = options.getService();
                 String text = "Hello, world!";
-                String targetLanguage = "zh_TW"; // 目標語言
+                String targetLanguage = "en"; // 目標語言
                 inputText.setText("輸入:"+result.get(0));
                 runTranslation(translate,result.get(0),targetLanguage);
             }
