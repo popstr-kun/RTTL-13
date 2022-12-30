48f320e7f9ccf4c901b102d68be94e11a86dde3d
SHEN XIAN KUN <106544049+popstr-kun@users.noreply.github.com>
2022-12-31 02:54:49 +0800

    語音識別與翻譯測試_語言輸出

--- app/src/main/java/com/example/rttl_13/MainActivity.java
+++ app/src/main/java/com/example/rttl_13/MainActivity.java
@@ -4,27 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.cloud.speech.v1.SpeechClient;
+
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
-
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         speak.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Speech to Text
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "zh-TW");
@@ -45,10 +46,18 @@ public class MainActivity extends AppCompatActivity {
                 }catch (ActivityNotFoundException a){
                     Toast.makeText(getApplicationContext(),"Intent problem", Toast.LENGTH_SHORT).show();
                 }
+
+                //Text to Speech
+                textToSpeech = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int i) {
+                        if(i != TextToSpeech.ERROR) {
+                            textToSpeech.setLanguage(Locale.US);
+                        }
+                    }
+                });
             }
         });
-
-        //
     }
 
     @Override
@@ -61,7 +70,8 @@ public class MainActivity extends AppCompatActivity {
                 TranslateOptions options = TranslateOptions.newBuilder().setApiKey("AIzaSyB1t4w5AJ3A2fOOacSWbYjj7peFyIXoYyg").build();
                 Translate translate = options.getService();
                 String text = "Hello, world!";
-                String targetLanguage = "en"; // 目標語言
+                // 目標語言
+                String targetLanguage = "en";
                 inputText.setText("輸入:"+result.get(0));
                 runTranslation(translate,result.get(0),targetLanguage);
             }
@@ -75,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    //必須使用
     private void runTranslation(Translate translate,String text,String targetLanguage){
         TextView outputText = findViewById(R.id.OutputText);
         new Thread(new Runnable() {
@@ -84,6 +95,12 @@ public class MainActivity extends AppCompatActivity {
                 String translatedText = translation.getTranslatedText();
                 System.out.println(translatedText);
                 outputText.setText("結果:"+translatedText);
+                try {
+                    Thread.sleep(50);
+                    textToSpeech.speak(translatedText,TextToSpeech.QUEUE_FLUSH,null);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }).start();
 
