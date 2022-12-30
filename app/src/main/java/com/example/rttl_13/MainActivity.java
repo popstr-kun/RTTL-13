59f833a2d5c258ad9f48cab3e5749137d06e0924
SHEN XIAN KUN <106544049+popstr-kun@users.noreply.github.com>
2022-12-31 07:24:02 +0800

    語音識別與翻譯測試_語言輸出2

--- app/src/main/java/com/example/rttl_13/MainActivity.java
+++ app/src/main/java/com/example/rttl_13/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.rttl_13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -11,6 +14,7 @@ import android.os.Message;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,7 +28,11 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    TextToSpeech textToSpeech;
+    private TextToSpeech textToSpeech;
+    private String repeatText;
+    Languages language = new Languages(Locale.TAIWAN,Locale.JAPAN);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +40,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView speak = findViewById(R.id.speak);
+        Button btnRepeat = findViewById(R.id.btn_repeat);
+        Button btnSwap = findViewById(R.id.btn_swap);
+
+        TextView inputText = findViewById(R.id.InputText);
+        inputText.setText(String.format("輸入(%s):",language.getInputLanguage()));
+        TextView outputText = findViewById(R.id.OutputText);
+        outputText.setText(String.format("結果(%s):",language.getOutputLanguage()));
 
         speak.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Speech to Text
+                // 判斷有無連接網路
+                ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+                NetworkInfo info = cm.getActiveNetworkInfo();
+                if( info == null || !info.isConnected() )
+                {
+                    Toast.makeText(getApplicationContext(),"無網路連接，請連接後重試", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "zh-TW");
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language.getInputLanguage());
                 intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "請說～");
                 try{
                     startActivityForResult(intent,200);
@@ -52,38 +76,42 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onInit(int i) {
                         if(i != TextToSpeech.ERROR) {
-                            textToSpeech.setLanguage(Locale.US);
+                            textToSpeech.setLanguage(language.getSpeechLanguage());
                         }
                     }
                 });
             }
         });
+
+        btnRepeat.setOnClickListener(v -> {
+            if(repeatText != null){
+                textToSpeech.speak(repeatText,TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+
+        btnSwap.setOnClickListener(v ->{
+            language.ioLanguageSwap();
+            inputText.setText(String.format("輸入(%s):",language.getInputLanguage()));
+            outputText.setText(String.format("結果(%s):",language.getOutputLanguage()));
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        TextView inputText = findViewById(R.id.InputText);
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == 200){
-            TextView inputText = findViewById(R.id.InputText);
             if(resultCode == RESULT_OK && data != null){
                 ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                 TranslateOptions options = TranslateOptions.newBuilder().setApiKey("AIzaSyB1t4w5AJ3A2fOOacSWbYjj7peFyIXoYyg").build();
                 Translate translate = options.getService();
-                String text = "Hello, world!";
-                // 目標語言
-                String targetLanguage = "en";
-                inputText.setText("輸入:"+result.get(0));
-                runTranslation(translate,result.get(0),targetLanguage);
+                inputText.setText(String.format("輸入(%s): %s",language.getInputLanguage(), result.get(0)));
+                runTranslation(translate,result.get(0),language.getOutputLanguage());
             }
         }
     }
 
-    private Handler handler = new Handler(Looper.myLooper()){
-        @Override
-        public void handleMessage(Message msg){
 
-        }
-    };
 
     //必須使用
     private void runTranslation(Translate translate,String text,String targetLanguage){
@@ -94,15 +122,69 @@ public class MainActivity extends AppCompatActivity {
                 Translation translation = translate.translate(text, Translate.TranslateOption.targetLanguage(targetLanguage));
                 String translatedText = translation.getTranslatedText();
                 System.out.println(translatedText);
-                outputText.setText("結果:"+translatedText);
+                outputText.setText(String.format("結果(%s): %s ",language.getOutputLanguage(),translatedText));
                 try {
-                    Thread.sleep(50);
-                    textToSpeech.speak(translatedText,TextToSpeech.QUEUE_FLUSH,null);
+                    Thread.sleep(100);
+                    if(translatedText != null){
+                        repeatText = translatedText;
+                        textToSpeech.speak(translatedText,TextToSpeech.QUEUE_FLUSH,null);
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }).start();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        textToSpeech.stop();
+    }
+
+}
+
+class Languages{
+    private String inputLanguage;
+    private String outputLanguage;
+    private Locale speechLanguage;
+
+    private Locale inputLocale;
+
+    Languages(Locale input,Locale output){
+        setInputLanguage(input);
+        setOutputLanguage(output);
+    }
+
+    void setInputLanguage(Locale language){
+        inputLanguage = language.toString();
+        inputLocale = inputLocale;
+    }
+    void setOutputLanguage(Locale language){
+        outputLanguage = language.toString();
+        speechLanguage = language;
+    }
+    String getInputLanguage(){
+        return inputLanguage;
+    }
+    String getOutputLanguage(){
+        return outputLanguage;
+    }
+    Locale getSpeechLanguage(){
+        return speechLanguage;
+    }
+
+    void ioLanguageSwap(){
+        String temp ;
+        Locale temp2 ;
+
+        temp = inputLanguage;
+        temp2 = inputLocale;
+        inputLanguage = outputLanguage;
+        inputLocale = speechLanguage;
+        outputLanguage = temp;
+        speechLanguage = temp2;
 
     }
 }
